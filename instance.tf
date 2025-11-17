a6d6edbb3b64eddb42bf800879e9ba0b5a341054
unknown <zmr.9841@gmail.com>
2025-11-17 12:25:42 +0530

    Added ec2, vpc with subnets

--- instance.tf
+++ instance.tf
@@ -1,11 +1,13 @@
-resource "aws_instance" "Server" {
-    ami = ""
-    instance_type = "t3.micro"
-    subnet_id = aws_subnet.public.id
-    vpc_security_group_ids = [ aws_security_group.public-sg.id ]
-    associate_public_ip_address = true
+# 7. EC2 Instance (Public)
+resource "aws_instance" "public_ec2" {
+  ami                         = "ami-02b8269d5e85954ef"
+  instance_type               = "t3.micro"
+  key_name                    = aws_key_pair.mykeypair-pub.key_name
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  associate_public_ip_address = true
 
-    tags = {
-        Name = "server"
-    }
+  tags = {
+    Name = "public-ec2"
+  }
 }
--- main.tf
+++ main.tf
@@ -1,87 +1,93 @@
+# 1. VPC
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = "Terraform-AWS"
-    }
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
+# 2. Public Subnet
 resource "aws_subnet" "public" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "ap-south-1a"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
-    tags = {
-      Name = "public-subnet"
-    }
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
+# 3. Private Subnet
 resource "aws_subnet" "private" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.2.0/24"
-    
-    tags = {
-      Name = "private-subnet"
-    }  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
 }
 
-
+# 4. Internet Gateway
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    tags = {
-        Name = "main-igw"
-    }
+  tags = {
+    Name = "main-igw"
+  }
 }
 
-resource "aws_route_table" "public-rt" {
-    vpc_id = aws_vpc.main.id
+# 5. Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
 
-    route = {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.id 
-    }
-    tags = {
-        Name = "public-rt"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
 }
 
-resource "aws_route_table_association" "public" {
-    subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.public-rt.id
-  
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_security_group" "public-sg" {
-    name = "public-sg"
-    vpc_id = aws_vpc.main.id
+# 6. Security Group
+resource "aws_security_group" "public_sg" {
+  name   = "public-sg"
+  vpc_id = aws_vpc.main.id
 
-    ingress = [ 
-      {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        description = "ssh"
-        cidr_blocks = ["0.0.0.0/0"]
-      },
-      {
-        from_port = 80
-        to_port =80
-        protocol = "tcp"
-        description = "http"
-        cidr_blocks = ["0.0.0.0/0"]
-      },
-      {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-      }
-    ]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "public-sg"
-    }     
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-sg"
+  }
 }
 
+resource "aws_key_pair" "mykeypair-pub" {
+  key_name   = "mykeypair-pub"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
